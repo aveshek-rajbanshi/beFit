24d5e910b5039490c74885913587b18f6d649f6c
Aveshek Rajbanshi <aveshrajbanshi@gmail.com>
2025-07-22 10:27:16 +0545

    commit-02 is to add content to home & dashboard page.

--- beFit/src/components/Dashboard.jsx
+++ beFit/src/components/Dashboard.jsx
@@ -5,7 +5,32 @@ const Dashboard = () => {
   return (
     <div className='content-main-page'>
         <Sidenavbar />
-        <h1>Dashboard Page.</h1>
+        <div className='dashboard-content-container'>
+        <div className='progress-LineChart'></div>
+        <div className='physicalActivity-wrapper'>
+        <div className='physical-left'>
+          <div className='physical-InfoContainer'>
+            <h4>Heart Beats</h4>
+            <h1>82 <span>Times</span></h1>
+          </div>
+          <div className='physical-InfoContainer'>
+            <h4>Calories</h4>
+            <h1>2000 <span>kcal</span></h1>
+          </div>
+          <div className='physical-InfoContainer'>
+            <h4>Walk</h4>
+            <h1>690 <span>Steps</span></h1>
+          </div>
+          <div className='physical-InfoContainer'></div>
+          <div className='physical-InfoContainer'></div>
+          <div className='physical-InfoContainer'></div>
+        </div>
+        <div className='physical-right'>
+          <h2>Activities Summery:</h2>
+        </div>
+        </div>
+
+        </div>
     </div>
   )
 }
--- beFit/src/components/Home.jsx
+++ beFit/src/components/Home.jsx
@@ -5,7 +5,20 @@ const Home = () => {
   return (
     <div className='content-main-page'>
     <Sidenavbar />
-        <h1>Home Page.</h1>
+        <div className='home-content-container'>
+        <div className='welcome-msg'>
+          <h1>Welcome Back!</h1>
+        </div>
+        <div className='progress-wrapper'>
+          <div className='progress-value'>
+            <p>You've achieved <br></br><span>60%  </span>of <br></br> Your weekly goals.</p>
+          </div>
+          <div className='motivate-msg'>
+            <p>Push yourself<br></br>because no one else is going to do it for you.</p>
+          </div>
+
+        </div>
+        </div>
     </div>
   )
 }
